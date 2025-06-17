bbb473d00ef4fda51bcff201428f5bdd5253e444
Nya Candy <dev@candinya.com>
2025-06-17 17:07:10 +0800

    fix: don't inject styles to prevent cors error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { resolve } from "path";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), libInjectCss()],
+  plugins: [preact()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "lib"),
