9ae135c4088640926f79526d7bdeda5d1862156a
Nya Candy <dev@candinya.com>
2025-06-18 23:33:17 +0800

    chore: tweak style

--- lib/components/Player/OpenToggle.tsx
+++ lib/components/Player/OpenToggle.tsx
@@ -1,11 +1,13 @@
 import styles from "./styles.module.css";
 import IconChevronRight from "@/icons/ChevronRight";
+import IconMusicNote from "@/icons/MusicNote";
 
 interface OpenToggleProps {
   toggle: () => void;
 }
 const OpenToggle = ({ toggle }: OpenToggleProps) => (
   <div className={styles.toggle} onClick={toggle}>
+    <IconMusicNote className={styles.icon} />
     <IconChevronRight className={styles.indicator} />
   </div>
 );
